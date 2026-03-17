d64f4e4057e7b298f9137daf173da64b3661722f
d2684066-s <email_used_in_d2684066-s_account>
2026-03-18 00:01:50 +0530

    Fix: Convert Django template tags to direct paths during build

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@
 # Create public directory
 mkdir -p public
 
-# Copy main HTML file
+# Copy main HTML file and convert Django template tags to direct paths
 cp RecipieGenerator/Template/Home.html public/index.html
+
+# Remove Django load tag and convert {% static 'xxx' %} to direct paths
+sed -i '' 's/{%[[:space:]]*load[[:space:]]*static[[:space:]]*%}//g' public/index.html
+sed -i '' "s/{%[[:space:]]*static[[:space:]]*['\"]\/\?\([^'\"]*\)['\"][[:space:]]*%}/\1/g" public/index.html
 
 # Copy static assets
 if [ -d "RecipieGenerator/assests" ]; then
-  mkdir -p public/assets
   cp -r RecipieGenerator/assests/* public/ 2>/dev/null || true
 fi
 
